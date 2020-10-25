31bb6755c9711da2ac3b1b69a680aba9ed93cdad
zehreken <zehreken@gmail.com>
2020-10-25 23:27:50 +0100

    Improve input example

--- src/bin/feedback/mod.rs
+++ src/bin/feedback/mod.rs
@@ -15,6 +15,9 @@ struct InputModel {
 struct Model {
     input_stream: audio::Stream<InputModel>,
     consumer: Consumer<f32>,
+    time_line_points: Vec<Point2>,
+    frequency_line_points: Vec<Point2>,
+    scale_points: Vec<Point2>,
 }
 
 fn model(app: &App) -> Model {
@@ -41,6 +44,9 @@ fn model(app: &App) -> Model {
     Model {
         input_stream,
         consumer,
+        time_line_points: vec![],
+        frequency_line_points: vec![],
+        scale_points: vec![],
     }
 }
 
@@ -66,7 +72,11 @@ fn update(_app: &App, model: &mut Model, _udpate: Update) {
         frames.push(recorded_sample);
     }
 
-    println!("frames count: {}", frames.len());
+    let (time_line, frequency_line, scale_points) = analyze(frames);
+    model.time_line_points = time_line;
+    model.frequency_line_points = frequency_line;
+    model.scale_points = scale_points;
+    // println!("frames count: {}", frames.len());
 
     std::thread::sleep(std::time::Duration::from_millis(33));
 }
@@ -75,4 +85,122 @@ fn view(app: &App, model: &Model, frame: Frame) {
     let draw = app.draw();
 
     draw.background().color(WHITE);
+
+    draw.polyline()
+        .weight(1.0)
+        .points(model.time_line_points.clone())
+        .color(CRIMSON);
+
+    // draw.polyline()
+    //     .weight(1.0)
+    //     .points(model.frequency_line_points.clone())
+    //     .color(GREEN);
+
+    if model.frequency_line_points.len() > 0 {
+        let average_bins = get_spectrum(model);
+        // draw.polyline().weight(1.0).points(average_bins).color(RED);
+        for bin in average_bins {
+            draw.rect()
+                .x_y(bin.x, -100.0)
+                .w_h(90.0, 200.0 - bin.y.abs())
+                .color(GREEN);
+        }
+    }
+
+    for (i, point) in model.scale_points.iter().enumerate() {
+        draw.rect().w_h(1.0, 10.0).xy(*point).color(BLACK);
+        let bin_text = i as f32 * BIN_SIZE * X_SCALE * 8.0;
+        draw.text(&format!("{:0.0}hz", bin_text))
+            .font_size(6)
+            .x_y(point.x, -80.0)
+            .color(BLACK);
+    }
+
+    draw.to_frame(app, &frame).unwrap();
+
+    draw.background().color(WHITE);
+}
+
+fn get_spectrum(model: &Model) -> Vec<Point2> {
+    // implement another view to have non-linear bin sizes
+    // e.g. 32-64-125-250-500-1k-2k-4k-8k-16k Hz
+    // get half of model.frequency_line_points
+    let mut sum = 1;
+    let bin_sizes: Vec<i32> = (0..9)
+        .map(|i| {
+            sum += 2_i32.pow(i);
+            sum
+        })
+        .collect();
+    // println!("bin_sizes: {:?}", bin_sizes);
+    // After this bin sizes are 4, 4, 8, 16, 32, 64, 128, 256. In total 512 data points, half of frequency_line_points
+    let mut bin_averages: Vec<Point2> = vec![];
+    let mut start_index = 0;
+    for (i, end_index) in bin_sizes.into_iter().enumerate() {
+        let sum: &f32 = &model.frequency_line_points[start_index as usize..end_index as usize]
+            .iter()
+            .map(|v| v.y)
+            .sum();
+        let average = sum / (end_index - start_index) as f32;
+        // println!("{} {} average: {}", start_index, end_index, average);
+        bin_averages.push(Point2 {
+            x: -462.0 + 100.0 * i as f32,
+            y: average,
+        });
+        start_index = end_index;
+    }
+
+    bin_averages
+}
+
+const BIN_SIZE: f32 = 22050.0 / 2048.0;
+const X_SCALE: f32 = 4.0;
+
+fn analyze(frames_slice: Vec<f32>) -> (Vec<Point2>, Vec<Point2>, Vec<Point2>) {
+    let sample_size = 1024 * 2;
+    let mut x = -513;
+    let time_line_points: Vec<Point2> = frames_slice
+        .iter()
+        .step_by(2) // Only visualize one channel
+        .map(|frame| {
+            x = x + 1;
+            Point2 {
+                x: x as f32,
+                y: 100.0 + frame * 1000.0,
+            }
+        })
+        .collect();
+
+    let input: Vec<_> = frames_slice
+        .iter()
+        .map(|frame| std::convert::From::from(*frame as f64))
+        .collect();
+
+    let output = kopek::fft::fft(&input);
+    let mut x = -512.0;
+    let frequency_line_points: Vec<Point2> = output
+        .iter()
+        .map(|c| {
+            let p = Point2 {
+                x,
+                y: -200.0 + ((c.re as f32).powf(2.0) + (c.im as f32).powf(2.0)).sqrt(),
+            };
+            x = x + 2048.0 / sample_size as f32 * X_SCALE;
+            p
+        })
+        .collect();
+
+    // First, the total range is 22050 if sample rate is 44100
+    // Frequency bin size is for each element in the output vector
+    // For example if the bin size is 22050 / 1024 = 21.53 and
+    // If the screen width is 1024, then each pixel will represent 21.53Hz
+    let scale_points: Vec<Point2> = (0..128)
+        .into_iter()
+        .map(|i| Point2 {
+            x: -512.0 + 8.0 * i as f32 * X_SCALE,
+            y: -100.0,
+        })
+        .collect();
+
+    (time_line_points, frequency_line_points, scale_points)
 }
