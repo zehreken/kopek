f604fa35d975327ee50e0f0ff0865383b300d0a9
zehreken <zehreken@gmail.com>
2023-12-21 17:23:52 +0100

    Clean up

--- examples/envelope/app.rs
+++ examples/envelope/app.rs
@@ -1,7 +1,6 @@
 use kopek::{
     envelope::*,
     oscillator::*,
-    time_signature::TimeSignature,
     utils::{C_FREQ, E_FREQ, G_FREQ},
 };
 use ringbuf::{HeapConsumer, HeapProducer};
@@ -15,9 +14,6 @@ pub struct App {
     input_consumer: HeapConsumer<Input>,
     view_producer: HeapProducer<ViewMessage>,
     envelope: Envelope,
-    time_4_4: TimeSignature,
-    time_3_4: TimeSignature,
-    time_5_4: TimeSignature,
 }
 
 impl App {
@@ -35,9 +31,6 @@ impl App {
             input_consumer,
             view_producer,
             envelope: Envelope::new(sample_rate, channel_count),
-            time_4_4: TimeSignature::new((4, 4), 120, 44100, 4),
-            time_3_4: TimeSignature::new((3, 4), 90, 44100, 4),
-            time_5_4: TimeSignature::new((5, 4), 75, 44100, 4),
         })
     }
 
@@ -45,22 +38,9 @@ impl App {
         for _ in 0..1024 {
             if !self.producer.is_full() {
                 let mut value = 0.0;
-                // let (show_4_4, accent) = self.time_4_4.update();
-                // if show_4_4 {
                 value += self.oscillator.sine(C_FREQ * 4.0, self.tick);
-                // }
 
                 value *= self.envelope.update();
-
-                // let (show_3_4, accent) = self.time_3_4.update();
-                // if show_3_4 {
-                //     value += self.oscillator.square(E_FREQ * 16.0, self.tick);
-                // }
-
-                // let (show_5_4, accent) = self.time_5_4.update();
-                // if show_5_4 {
-                //     value += self.oscillator.triangle(G_FREQ * 16.0, self.tick);
-                // }
 
                 self.producer.push(value).unwrap();
                 self.tick += 1.0;
@@ -79,16 +59,5 @@ impl App {
                 .push(ViewMessage::State(self.envelope.get_state()))
                 .unwrap();
         }
-        // if self.view_producer.free_len() >= 3 {
-        //     self.view_producer
-        //         .push(ViewMessage::Beat4_4(self.time_4_4.get_beat_index()))
-        //         .unwrap();
-        //     self.view_producer
-        //         .push(ViewMessage::Beat3_4(self.time_3_4.get_beat_index()))
-        //         .unwrap();
-        //     self.view_producer
-        //         .push(ViewMessage::Beat5_4(self.time_5_4.get_beat_index()))
-        //         .unwrap();
-        // }
     }
 }
