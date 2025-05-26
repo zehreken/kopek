6c1b39a11c4e8d368335a28389a7d2175cad1049
zehreken <zehreken@gmail.com>
2025-05-26 11:08:28 +0200

    Refactor

--- src/utils.rs
+++ src/utils.rs
@@ -115,3 +115,30 @@ pub const KEYS: [(&str, Key); 13] = [
     ("B", Key::B),
     ("_", Key::Rest),
 ];
+
+#[derive(Debug, Clone, Copy, PartialEq)]
+pub struct Note {
+    pub octave: Octave,
+    pub key: Key,
+}
+
+impl Note {
+    fn octave(&self) -> f32 {
+        let shift = match self.octave {
+            Octave::First => 0,
+            Octave::Second => 1,
+            Octave::Third => 2,
+            Octave::Fourth => 3,
+            Octave::Fifth => 4,
+        };
+        2.0_f32.powi(shift)
+    }
+
+    fn frequency(&self) -> f32 {
+        key_to_frequency(self.key)
+    }
+
+    pub fn get(&self) -> f32 {
+        self.octave() * self.frequency()
+    }
+}
